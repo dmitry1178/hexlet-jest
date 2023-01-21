bba3bf97a29508de0e763dafa41f4aa9876066a8
Dmitry Nesterov <media.dn11@gmail.com>
2023-01-21 16:15:25 +0300

    add set.test.js

---
+++ __tests__/set.test.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+import _ from 'lodash';
+
+import getFunction from '../functions.js';
+
+const set = getFunction();
+
+// BEGIN (write your solution here)
+
+let data;
+let dataCopy;
+
+beforeEach(() => {
+data = {a: [{ b: { c: 3 } }] };
+dataCopy = _.cloneDeep(data);
+});
+
+test('plain set', () => {
+set(data, 'a', 'value');
+dataCopy.a = 'value';
+expect(data).toEqual(dataCopy);
+});
+
+test('nested set', () => {
+set(data, 'a[0].b.c', true);
+dataCopy.a[0].b.c = true;
+expect(data).toEqual(dataCopy);
+});
+
+test('set new property', () => {
+set(data, 'a[0].b.d', false);
+dataCopy.a[0].b.d = false;
+expect(data).toEqual(dataCopy);
+});
